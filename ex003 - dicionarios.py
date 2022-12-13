6744ce28e17538c859d040f4970ac6788463d5b7
Gustavo Matte <gustavo.matte@hotmail.com>
2022-12-12 22:40:36 -0300

    ex003 commit

---
+++ ex003 - dicionarios.py
@@ -0,0 +1,5 @@
+# Considere o seguinte dicionário: {'m1': {'m2': 'Olá Mundo'}}.
+# Carregue e apresente a mensagem "Olá Mundo" contida no dicionário
+
+d = {'m1': {'m2': 'Olá Mundo'}}
+print(d['m1']['m2'])
