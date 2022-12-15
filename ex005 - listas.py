42fd7e1d3e1a129fda73a3970b1cdbb2ab66d1fb
Gustavo Matte <gustavo.matte@hotmail.com>
2022-12-14 21:10:37 -0300

    reajuste

--- ex005 - listas.py
+++ ex005 - listas.py
@@ -1,13 +1,11 @@
 # Desenvolva um programa que armazene quatro notas em uma lista e que apresente:
 # a média final, a maior nota e a menor nota
 
-lista = []
-soma = 0
+notas = []
 print('ARMAZENANDO 4 NOTAS')
 for c in range(1, 5):
-    nota = (float(input(f'Informe a {c}ª nota: ')))
-    soma += nota
-    lista.append(nota)
+    notas.append(float(input(f'Informe a {c}ª nota: ')))
+
 print('-' * 20)
-média = soma / c
-print(f'{lista}\nMédia = {média}\nMaior nota = {max(lista)}\nMenor nota = {min(lista)}')
+média = sum(notas) / len(notas)
+print(f'{notas}\nMédia = {média}\nMaior nota = {max(notas)}\nMenor nota = {min(notas)}')
