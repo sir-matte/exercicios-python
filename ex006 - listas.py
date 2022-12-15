a9d3361567dccc3c97396e3f02be7000e5a4b065
Gustavo Matte <gustavo.matte@hotmail.com>
2022-12-14 21:35:17 -0300

    ex006 adjust

--- ex006 - listas.py
+++ ex006 - listas.py
@@ -4,7 +4,6 @@
 # apresentar a mensagem "APROVADO", caso contrário, apresentar a mensagem "REPROVADO"
 
 notas = []
-recuperacao = []
 for c in range(1, 5):
     notas.append(float(input(f'Informe a {c}ª nota: ')))
 media = sum(notas) / len(notas)
@@ -12,10 +11,8 @@ print(f'\t{notas}\n\tMédia final = {media:.1f}')
 if media >= 7:
     print('\t\033[32mAPROVADO!\033[m')
 else:
-    print('\t\033[31mREPROVADO! Faça a prova final.\033[m')
-    media = 0
-    for c in range(1, 5):
-        recuperacao.append(float(input(f'Informe a {c}ª nota da prova final: ')))
-    media = sum(recuperacao) / len(recuperacao)
-    print(f'\t{recuperacao}\n\tMédia da prova final = {media:.1f}')
-    print('\t\033[32mAPROVADO!\033[m' if media >= 5 else '\t\033[31mREPROVADO!!!\033[m')
+    print('\t\033[33mFaça a prova final.\033[m')
+    notas.append(float(input(f'Informe a nota da prova final: ')))
+    media = sum(notas) / len(notas)
+    print(f'\tMédia final = {media:.1f}')
+    print('\t\033[32mAPROVADO!\033[m' if media >= 5 else '\t\033[31mREPROVADO!\033[m')
